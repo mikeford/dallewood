a64d2f4df7303228e38fc8391ae3c4d9796e7b1f
Mike Ford <mikeford@users.noreply.github.com>
2022-08-16 00:52:01 -0400

    Clean up cobra menu placeholders

--- cmd/zoom.go
+++ cmd/zoom.go
@@ -30,12 +30,7 @@ var easingMode render.AnimationEasingMode
 var zoomCmd = &cobra.Command{
 	Use:   "zoom",
 	Short: "Create an infinite zoom video using progressively outcropped images",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Long:  `Create an infinite zoom video using progressively outcropped images`,
 	Run: func(cmd *cobra.Command, args []string) {
 		if len(args) == 0 {
 			panic("Incorrect invocation: Must specify directory containing input frames")
--- cmd/zoom_downsize.go
+++ cmd/zoom_downsize.go
@@ -27,12 +27,7 @@ import (
 var downsizeCmd = &cobra.Command{
 	Use:   "downsize",
 	Short: "Downsize image for infinite zoom outcropping",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Long:  `Downsize image for infinite zoom outcropping`,
 	Run: func(cmd *cobra.Command, args []string) {
 		if len(args) == 0 {
 			panic("Incorrect invocation: Must specify image file to downsize")
